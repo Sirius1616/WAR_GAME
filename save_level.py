659be900777e6b4c28c76edc02001611490a06e8
Sirius1616 <siriusa1.615@gmail.com>
2026-05-12 23:11:39 +0100

    Including necessary files to repo

---
+++ save_level.py
@@ -0,0 +1,34 @@
+import json
+from pathlib import Path
+
+
+
+def save_natas():
+    PATH = Path("natas_progress.json")
+    file_name = PATH.name
+
+    level = input("Please enter the level: ")
+    password = input("Please enter the password for next level: ")
+    Lesson = input("Tell us th econcept you leanrt today: ")
+    commands = input("Commands used to pass through: ")
+    
+    if PATH.exists() and PATH.stat().st_size > 0:
+        print("Yes")
+        with open(file_name, 'r') as f:
+            dict = json.load(f)
+    else:
+        PATH = Path.touch(file_name)
+        dict = {}
+        print('not')
+
+    dict[level] = {"password": password,
+                   "lesson": Lesson,
+                   "commands": commands
+        } 
+    
+    with open(file_name, 'w') as f:
+            json.dump(dict, f, indent=4)
+    return dict
+
+
+save_natas()
